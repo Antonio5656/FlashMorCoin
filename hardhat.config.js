99af23eff68be4097a3886ff3500a70c1737f48e
Multiplataforma <tonoviveros@gmail.com>
2025-11-10 11:12:33 -0600

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,15 +17,36 @@ module.exports = {
   networks: {
     hardhat: {},
     mumbai: {
+
+
+
       url: API_URL || "https://polygon-mumbai.infura.io/v3/YOUR-PROJECT-ID",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+POLYGON_MUMBAI_URL=https://polygon-mumbai.infura.io/v3/4df8eead51294cd09eadf4b51efaa014
+POLYGONSCAN_API_KEY=01b_Lg_ZvjpnKoQGgqGlT
+
+
     },
     sepolia: {
       url: API_URL || "https://sepolia.infura.io/v3/YOUR-PROJECT-ID",
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      accounts: PRIVATE_KEY ? [4df8eead51294cd09eadf4b51efaa014] : [],
+
+# Si usas Sepolia:
+SEPOLIA_URL=https://sepolia.infura.io/v3/4df8eead51294cd09eadf4b51efaa014
+ETHERSCAN_API_KEY=your_etherscan_api_key_here
+
     }
   },
+
   etherscan: {
+
+
     apiKey: process.env.POLYGONSCAN_API_KEY || process.env.ETHERSCAN_API_KEY
+
+      # Paste this in your .env file
+NEXT_PUBLIC_ALCHEMY_API_KEY=01b_Lg_ZvjpnKoQGgqGlT
+NEXT_PUBLIC_ALCHEMY_POLICY_ID=4b1409cc-8f15-4566-92b8-a397ea6f79e0
+
+
   }
 };
